1aaf4b050256f1268a9dc1ae0c2e94449668b9d8
root <root@ip-172-31-2-77.ca-central-1.compute.internal>
2022-05-22 21:50:30 +0000

    new db updated

--- src/main/java/com/example/DbUtil.java
+++ src/main/java/com/example/DbUtil.java
@@ -9,9 +9,9 @@ import java.sql.SQLException;
 public class DbUtil {
 	static Connection con=null;
 	static String driver = "com.mysql.cj.jdbc.Driver";
-	static String url = "jdbc:mysql://lwplabs.cojh5a5pj8rf.us-east-1.rds.amazonaws.com:3306/lwplabs";
-	static String username = "master";
-	static String password = "welcome1";
+	static String url = "jdbc:mysql://learnproject.cwcg6ly3jhus.ca-central-1.rds.amazonaws.com:3306/learnproject";
+	static String username = "admin";
+	static String password = "biren1801";
 	
 	static {
 		try {
